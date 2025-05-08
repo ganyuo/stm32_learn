ceb088608662b2c8b4f8f4db20526301ce297293
ganyuo <ganyuo@outlook.com>
2025-05-09 01:09:48 +0800

    改成TI2FP2从模式

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -51,9 +51,8 @@ void MX_TIM2_Init(void)
         Error_Handler();
     }
     sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
-    sSlaveConfig.InputTrigger = TIM_TS_ETRF;
-    sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_NONINVERTED;
-    sSlaveConfig.TriggerPrescaler = TIM_TRIGGERPRESCALER_DIV1;
+    sSlaveConfig.InputTrigger = TIM_TS_TI2FP2;
+    sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
     sSlaveConfig.TriggerFilter = 15;
     if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
     {
@@ -85,9 +84,9 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
 
         __HAL_RCC_GPIOA_CLK_ENABLE();
         /**TIM2 GPIO Configuration
-        PA0-WKUP     ------> TIM2_ETR
+        PA1     ------> TIM2_CH2
         */
-        GPIO_InitStruct.Pin = GPIO_PIN_0;
+        GPIO_InitStruct.Pin = GPIO_PIN_1;
         GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -110,9 +109,9 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
         __HAL_RCC_TIM2_CLK_DISABLE();
 
         /**TIM2 GPIO Configuration
-        PA0-WKUP     ------> TIM2_ETR
+        PA1     ------> TIM2_CH2
         */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0);
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
 
         /* USER CODE BEGIN TIM2_MspDeInit 1 */
 
